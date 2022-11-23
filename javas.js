22f056a22896a689ca8015aef8a9c136e83ca628
EmilianoCanto <118865786+EmilianoCanto@users.noreply.github.com>
2022-11-22 19:46:02 -0600

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,8 @@
+function drum (){
+    document.getElementById("bateria").play();
+    document.getElementById("music").innerHTML="refresca la pagina para dejar de escuchar";
+}
+   function guardians (){
+    document.getElementById("galaxy").play();
+    document.getElementById("music").innerHTML="refresca la pagina para dejar de escuchar";
+   }
